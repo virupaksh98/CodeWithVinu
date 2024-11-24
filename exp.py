6a5796df8c70b524f562cd6c863c38abb4d534e4
virupaksh98 <81576159+virupaksh98@users.noreply.github.com>
2024-11-24 23:21:54 +0530

    Add files via upload

--- exp.py
+++ exp.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-"""
+
+
 data =  {
     'Name':['Abhi','Siddhi','Chetan'],
     'Age': [26,24,30],
@@ -8,12 +9,27 @@ data =  {
 }
 
 df = pd.DataFrame(data)
-print(df)   
-"""
+"""print(df)"""   
 
-df = pd.read_csv('data.csv')
+
+"""df = pd.read_csv('data.csv')
 print(df.head())
 print(df.tail())
 
 print(df.info())
 print(df.describe())
+
+print(df.get('Email_ID'))
+
+print(df.iloc[0])
+
+filter_df = df[df['Emp#'] >= 58209 ]"""
+
+df = df.drop(columns=['Name'])
+
+
+df = df.drop(index=0)
+
+print(df.head())
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import try1 as t1
+
+result = t1.add(5,3)
+print(result)
+
+dif = 
